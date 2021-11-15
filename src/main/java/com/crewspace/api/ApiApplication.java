8fb732af114e3ca3b120c9995e39c4ac54ccdc03
yaeoni <tjghwnd103@gmail.com>
2021-11-16 02:08:33 +0900

    [CHORE] 환경변수 yml 추가

--- src/main/java/com/crewspace/api/ApiApplication.java
+++ src/main/java/com/crewspace/api/ApiApplication.java
@@ -2,12 +2,20 @@ package com.crewspace.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ApiApplication {
 
+	public static final String APPLICATION_LOCATIONS =
+		"spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:env.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		new SpringApplicationBuilder(ApiApplication.class)
+			.properties(APPLICATION_LOCATIONS)
+			.run(args);
 	}
 
 }
