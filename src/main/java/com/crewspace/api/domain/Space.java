463d78f71f0a2d1d8848058ae10627f47aa0424a
yaeoni <tjghwnd103@gmail.com>
2021-11-19 03:00:41 +0900

    [ADD] 동아리 생성 메서드

--- src/main/java/com/crewspace/api/domain/Space.java
+++ src/main/java/com/crewspace/api/domain/Space.java
@@ -1,9 +1,12 @@
 package com.crewspace.api.domain;
 
+import java.util.Locale;
+import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,4 +29,26 @@ public class Space extends BaseTimeEntity{
     private Boolean hasContact;
     private Boolean hasSns;
     private Boolean hasEtc;
+
+    // 생성 메서드
+    @Builder
+    public Space(String image, String name, String description, Boolean hasBirthdate,
+        Boolean hasEmail, Boolean hasContact, Boolean hasSns, Boolean hasEtc) {
+
+        // 초대 코드 랜덤 생성
+        String invitationCode = UUID.randomUUID().toString().replaceAll("-", "");
+        invitationCode = invitationCode.substring(0, 6).toUpperCase();
+
+        this.invitationCode = invitationCode;
+
+        this.image = image;
+        this.name = name;
+        this.description = description;
+
+        this.hasBirthdate = hasBirthdate;
+        this.hasEmail = hasEmail;
+        this.hasContact = hasContact;
+        this.hasSns = hasSns;
+        this.hasEtc = hasEtc;
+    }
 }
