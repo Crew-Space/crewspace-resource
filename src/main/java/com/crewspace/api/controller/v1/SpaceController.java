d96833733bbd2b72d1569ef9963a1d23fa211643
yaeoni <tjghwnd103@gmail.com>
2021-11-20 04:49:24 +0900

    [FEAT]  초대코드 입력 Controller 계층 개발 / DTO 매핑

--- src/main/java/com/crewspace/api/controller/v1/SpaceController.java
+++ src/main/java/com/crewspace/api/controller/v1/SpaceController.java
@@ -2,15 +2,20 @@ package com.crewspace.api.controller.v1;
 
 import com.crewspace.api.constants.SuccessCode;
 import com.crewspace.api.dto.req.space.CreateSpaceRequest;
+import com.crewspace.api.dto.req.space.InvitationCodeRequestDTO;
 import com.crewspace.api.dto.res.space.CreateSpaceResponse;
 import com.crewspace.api.dto.res.space.CreateSpaceResponseDTO;
+import com.crewspace.api.dto.res.space.InvitationCodeResponse;
+import com.crewspace.api.dto.res.space.InvitationCodeResponseDTO;
 import com.crewspace.api.service.SpaceService;
 import com.crewspace.api.utils.SecurityUtil;
 import java.io.IOException;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +42,14 @@ public class SpaceController {
 
         CreateSpaceResponseDTO responseDTO = spaceService.create(request.toCreateSpaceDTO(imageURL, memberEmail));
         return CreateSpaceResponse.toResponse(SuccessCode.CREATE_SPACE_SUCCESS, responseDTO);
+    }
+
+    @GetMapping("/space/{space-code}")
+    public ResponseEntity<InvitationCodeResponse> confirmInvitationCode(@PathVariable("space-code") String spaceCode){
+
+        InvitationCodeRequestDTO invitationCodeRequestDTO = new InvitationCodeRequestDTO(spaceCode);
+        InvitationCodeResponseDTO responseDTO = spaceService.confirmInvitationCode(invitationCodeRequestDTO);
+
+        return InvitationCodeResponse.toResponse(SuccessCode.VALID_SPACE_CODE, responseDTO);
     }
 }
