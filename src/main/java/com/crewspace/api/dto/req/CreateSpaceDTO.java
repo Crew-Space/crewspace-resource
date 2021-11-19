f09be9547696c3b86ad48054f882f9a4ad0afba5
yaeoni <tjghwnd103@gmail.com>
2021-11-20 02:10:47 +0900

    [FEAT] 동아리 생성 service 계층 개발 / DTO 생성
    controller와 service에서 받는 DTO / Service 계층에서 다루는 DTO 에서 entity 변환로직 삽입

---
+++ src/main/java/com/crewspace/api/dto/req/CreateSpaceDTO.java
@@ -0,0 +1,67 @@
+package com.crewspace.api.dto.req;
+
+import com.crewspace.api.domain.member.Member;
+import com.crewspace.api.domain.post.PostCategory;
+import com.crewspace.api.domain.space.Space;
+import com.crewspace.api.domain.spaceMember.MemberCategory;
+import com.crewspace.api.domain.spaceMember.SpaceMember;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateSpaceDTO {
+
+    private String memberEmail;
+
+    private String image;
+    private String name;
+    private String description;
+    private List<String> memberCategory;
+
+    private Boolean hasBirthdate;
+    private Boolean hasEmail;
+    private Boolean hasContact;
+    private Boolean hasSns;
+    private Boolean hasEtc;
+
+    public Space toSpace(){
+        return Space.builder()
+            .image(image)
+            .name(name)
+            .description(description)
+            .hasBirthdate(hasBirthdate)
+            .hasEmail(hasEmail)
+            .hasContact(hasContact)
+            .hasSns(hasSns)
+            .hasEtc(hasEtc)
+            .build();
+    }
+
+    public MemberCategory toMemberCategory(Space space, String categoryName, Boolean isAdmin){
+        return MemberCategory.of(space, categoryName, isAdmin);
+    }
+
+    public PostCategory toPostCategory(Space space, String categoryName, Boolean isNotice){
+        return PostCategory.of(space, categoryName, isNotice);
+    }
+    
+    public SpaceMember toSpaceMember(Space space, Member member, MemberCategory adminCategory){
+        return SpaceMember.builder()
+            .space(space)
+            .member(member)
+            .memberCategory(adminCategory)
+            .image(member.getImage())
+            .name(member.getNickname())
+            .description(space.getName() + "의 운영진입니다 :)")
+            .build();
+    }
+
+}
---
+++ src/main/java/com/crewspace/api/service/SpaceService.java
@@ -0,0 +1,57 @@
+package com.crewspace.api.service;
+
+import com.crewspace.api.constants.ExceptionCode;
+import com.crewspace.api.domain.member.Member;
+import com.crewspace.api.domain.member.MemberRepository;
+import com.crewspace.api.domain.post.PostCategory;
+import com.crewspace.api.domain.post.PostCategoryRepository;
+import com.crewspace.api.domain.space.Space;
+import com.crewspace.api.domain.space.SpaceRepository;
+import com.crewspace.api.domain.spaceMember.MemberCategory;
+import com.crewspace.api.domain.spaceMember.MemberCategoryRepository;
+import com.crewspace.api.domain.spaceMember.SpaceMember;
+import com.crewspace.api.domain.spaceMember.SpaceMemberRepository;
+import com.crewspace.api.dto.req.CreateSpaceDTO;
+import com.crewspace.api.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SpaceService {
+
+    private final MemberRepository memberRepository;
+    private final SpaceRepository spaceRepository;
+    private final SpaceMemberRepository spaceMemberRepository;
+    private final MemberCategoryRepository memberCategoryRepository;
+    private final PostCategoryRepository postCategoryRepository;
+
+    @Transactional
+    public Space create(CreateSpaceDTO createSpaceDTO) {
+
+        Space space = createSpaceDTO.toSpace();
+        spaceRepository.save(space);
+
+        // 게시글 카테고리 생성 (공지 : 일반공지, 커뮤니티 : 일반)
+        createSpaceDTO.toPostCategory(space, "일반 공지", true);
+        createSpaceDTO.toPostCategory(space, "일반", false);
+
+        // 회원 카테고리 생성 (운영진 + 받은 값)
+        MemberCategory adminCategory = createSpaceDTO.toMemberCategory(space, "운영진", true);
+        memberCategoryRepository.save(adminCategory);
+
+        createSpaceDTO.getMemberCategory().stream()
+            .map(categoryName -> memberCategoryRepository.save(createSpaceDTO.toMemberCategory(space, categoryName, false)));
+
+        // 동아리_회원 운영진으로 자동 가입
+        Member member = memberRepository.findByEmail(createSpaceDTO.getMemberEmail())
+            .orElseThrow(() -> new CustomException(ExceptionCode.MEMBER_EMAIL_NOT_FOUND));
+
+        SpaceMember spaceMember = createSpaceDTO.toSpaceMember(space, member, adminCategory);
+        spaceMemberRepository.save(spaceMember);
+
+        return space;
+    }
+}
