daed03d196d335ee4112cc771ead98f66eda309d
yaeoni <tjghwnd103@gmail.com>
2021-11-19 16:21:29 +0900

    [FEAT] 현재 로그인한 유저정보 불러오기 (Security Context)

---
+++ src/main/java/com/crewspace/api/utils/SecurityUtil.java
@@ -0,0 +1,20 @@
+package com.crewspace.api.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtil {
+    private SecurityUtil() { }
+
+    // SecurityContext 에 저장된 유저 정보 불러오기
+    public static String getCurrentMemberId() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getName() == null) {
+            throw  new RuntimeException("Security Context 에 인증 정보가 없습니다.");
+        }
+
+        return authentication.getName();
+    }
+
+}
