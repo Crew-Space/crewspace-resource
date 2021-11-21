7d952f97617aec921d8f7a5dbb18aaed415467d5
yaeoni <tjghwnd103@gmail.com>
2021-11-22 02:42:32 +0900

    [FEAT] 가입한 동아리 목록 조회 Fetch join 이용하기

--- src/main/java/com/crewspace/api/domain/spaceMember/SpaceMemberRepository.java
+++ src/main/java/com/crewspace/api/domain/spaceMember/SpaceMemberRepository.java
@@ -1,10 +1,11 @@
 package com.crewspace.api.domain.spaceMember;
 
 import com.crewspace.api.domain.member.Member;
-import com.crewspace.api.domain.space.Space;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SpaceMemberRepository extends JpaRepository<SpaceMember, Long> {
+    @Query("select m from SpaceMember m join fetch m.space ")
     List<SpaceMember> findByMember(Member member);
 }
