946ab9b9873ed073490f72ac3176037ace5cb569
yaeoni <tjghwnd103@gmail.com>
2021-11-23 05:46:45 +0900

    [FEAT] 카테고리 생성 Service 계층 개발 / DTO 매핑

---
+++ src/main/java/com/crewspace/api/service/PostCategoryService.java
@@ -0,0 +1,40 @@
+package com.crewspace.api.service;
+
+import static com.crewspace.api.constants.ExceptionCode.*;
+
+import com.crewspace.api.constants.ExceptionCode;
+import com.crewspace.api.controller.v1.PostCategoryController;
+import com.crewspace.api.domain.post.PostCategory;
+import com.crewspace.api.domain.post.PostCategoryRepository;
+import com.crewspace.api.domain.spaceMember.SpaceMember;
+import com.crewspace.api.domain.spaceMember.SpaceMemberRepository;
+import com.crewspace.api.dto.req.postCategory.CreatePostCategoryRequestDTO;
+import com.crewspace.api.exception.CustomException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PostCategoryService {
+
+    private final PostCategoryRepository postCategoryRepository;
+    private final SpaceMemberRepository spaceMemberRepository;
+
+    @Transactional
+    public void create(CreatePostCategoryRequestDTO request){
+        SpaceMember spaceMember = spaceMemberRepository.findBySpaceIdAndMemberEmail(
+                request.getSpaceId(), request.getMemberEmail())
+            .orElseThrow(() -> new CustomException(SPACE_MEMBER_NOT_FOUND));
+
+        if(!spaceMember.getIsAdmin()) throw new CustomException(SPACE_ADMIN_ONLY);
+
+        List<PostCategory> postCategories = request.toPostCategory(spaceMember.getSpace());
+        postCategoryRepository.saveAll(postCategories);
+
+        return;
+    }
+}
+
