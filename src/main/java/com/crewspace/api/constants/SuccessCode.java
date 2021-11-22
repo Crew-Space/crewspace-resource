8dadaba1d42d0702238863a871ac6139e3520d43
yaeoni <tjghwnd103@gmail.com>
2021-11-23 06:18:42 +0900

    [ADD] 게시글 카테고리 조회 Success code 추가

--- src/main/java/com/crewspace/api/constants/SuccessCode.java
+++ src/main/java/com/crewspace/api/constants/SuccessCode.java
@@ -14,7 +14,8 @@ public enum SuccessCode {
     LOAD_REGISTER_INFO_SUCCESS(OK, "동아리 회원 가입 정보를 로드했습니다."),
     ENTER_SPACE_SUCEESS(OK, "동아리 가입을 성공했습니다."),
     LOAD_SPACES_SUCCESS(OK, "가입된 동아리 리스트를 불러왔습니다."),
-    CREATE_CATEGORY_SUCCESS(OK, "카테고리 생성에 성공했습니다");
+    CREATE_CATEGORY_SUCCESS(OK, "카테고리 생성에 성공했습니다"),
+    LOAD_POST_CATEGORIES_SUCCES(OK, "게시글 카테고리 조회에 성공했습니다.");
 
     private final HttpStatus status;
     private final String msg;
