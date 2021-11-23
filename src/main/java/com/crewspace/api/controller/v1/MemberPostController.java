ee9e8cf04de933a8c8fe8642752f18d6baf2c088
yaeoni <tjghwnd103@gmail.com>
2021-11-23 21:37:48 +0900

    [FEAT] 게시글 저장 Controller 계층 개발 / DTO 매핑

---
+++ src/main/java/com/crewspace/api/controller/v1/MemberPostController.java
@@ -0,0 +1,34 @@
+package com.crewspace.api.controller.v1;
+
+import static com.crewspace.api.constants.SuccessCode.SAVE_POST_SUCCESS;
+
+import com.crewspace.api.constants.SuccessCode;
+import com.crewspace.api.dto.req.memberPost.SavePostRequestDTO;
+import com.crewspace.api.dto.res.memberPost.MemberPostResponse;
+import com.crewspace.api.service.MemberPostService;
+import com.crewspace.api.utils.SecurityUtil;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberPostController {
+
+    private final MemberPostService memberPostService;
+
+    @PostMapping("/v1/posts/{post-id}/save")
+    public ResponseEntity<MemberPostResponse> savePost(
+        @PathVariable("post-id") Long postId, @Valid @RequestHeader("Space-Id") Long spaceId){
+
+        String memberEmail = SecurityUtil.getCurrentMemberId();
+        SavePostRequestDTO requestDTO = SavePostRequestDTO.of(spaceId, memberEmail, postId);
+        memberPostService.save(requestDTO);
+
+        return MemberPostResponse.newResponse(SAVE_POST_SUCCESS);
+    }
+}
