1763333ccc9b832fb078bbaac0232b2bbdbc04e5
yaeoni <tjghwnd103@gmail.com>
2021-11-25 21:40:34 +0900

    [ADD] 회원 프로필 상세 조회 Success, Exception 코드 추가

--- src/main/java/com/crewspace/api/constants/SuccessCode.java
+++ src/main/java/com/crewspace/api/constants/SuccessCode.java
@@ -29,7 +29,8 @@ public enum SuccessCode {
     LOAD_COMMUNITY_LIST_SUCCESS(OK, "커뮤니티 게시글 로딩을 성공했습니다!"),
     LOAD_NOTICE_LIST_SUCCESS(OK, "공지 게시글 로딩을 성공했습니다!"),
     LOAD_MEMBER_LIST_SUCCESS(OK, "회원 리스트 로딩을 성공했습니다!"),
-    SEARCH_MEMBER_LIST_SUCCESS(OK, "회원 검색을 성공했습니다!");
+    SEARCH_MEMBER_LIST_SUCCESS(OK, "회원 검색을 성공했습니다!"),
+    MEMBER_INFO_SUCCESS(OK, "회원 프로필 상세 조회를 성공했습니다~!");
 
     private final HttpStatus status;
     private final String msg;
