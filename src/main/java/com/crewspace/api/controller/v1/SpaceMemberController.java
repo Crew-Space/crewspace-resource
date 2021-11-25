d9201788367197c8b08bd3e06c2e0a59c9d70d33
yaeoni <tjghwnd103@gmail.com>
2021-11-25 17:37:11 +0900

    [FEAT] 회원 프로필 목록 조회 Controller 계층 개발 / DTO 매핑

--- src/main/java/com/crewspace/api/controller/v1/SpaceMemberController.java
+++ src/main/java/com/crewspace/api/controller/v1/SpaceMemberController.java
@@ -1,14 +1,22 @@
 package com.crewspace.api.controller.v1;
 
+import static com.crewspace.api.constants.SuccessCode.*;
+
 import com.crewspace.api.constants.SuccessCode;
 import com.crewspace.api.dto.req.spaceMember.EnteredSpaceRequestDTO;
+import com.crewspace.api.dto.req.spaceMember.MemberListRequestDTO;
 import com.crewspace.api.dto.res.spaceMember.EnteredSpaceResponse;
 import com.crewspace.api.dto.res.spaceMember.EnteredSpaceResponseDTO;
+import com.crewspace.api.dto.res.spaceMember.SpaceMemberListResponse;
+import com.crewspace.api.dto.res.spaceMember.SpaceMemberListResponseDTO;
 import com.crewspace.api.service.SpaceMemberService;
 import com.crewspace.api.utils.SecurityUtil;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,7 +32,19 @@ public class SpaceMemberController {
         EnteredSpaceRequestDTO enteredSpaceRequestDTO = new EnteredSpaceRequestDTO(memberEmail);
         EnteredSpaceResponseDTO responseDTO = spaceMemberService.enteredSpaceList(enteredSpaceRequestDTO);
 
-        return EnteredSpaceResponse.toResponse(SuccessCode.LOAD_SPACES_SUCCESS, responseDTO);
+        return EnteredSpaceResponse.toResponse(LOAD_SPACES_SUCCESS, responseDTO);
     }
 
+    @GetMapping("/v1/members")
+    public ResponseEntity<SpaceMemberListResponse> memberList(@Valid @RequestHeader("Space-Id") Long spaceId,
+        @RequestParam(value = "memberCategoryId", defaultValue = "-1", required = false) Long memberCategoryId){
+
+        String memberEmail = SecurityUtil.getCurrentMemberId();
+
+        MemberListRequestDTO requestDTO = MemberListRequestDTO.of(spaceId, memberEmail, memberCategoryId);
+
+        SpaceMemberListResponseDTO responseDTO = spaceMemberService.memberList(requestDTO);
+
+        return SpaceMemberListResponse.newResponse(LOAD_MEMBER_LIST_SUCCESS, responseDTO);
+    }
 }
