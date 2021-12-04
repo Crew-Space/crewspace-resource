1ba147a68983fa9e6a94494b8e3d2853287cf48c
yaeoni <tjghwnd103@gmail.com>
2021-12-05 07:14:25 +0900

    [CHORE] 환경 변수 변경

--- src/main/java/com/crewspace/api/InitDb.java
+++ src/main/java/com/crewspace/api/InitDb.java
@@ -91,12 +91,6 @@ public class InitDb {
             Member member11 = new Member("11", "suhan123@spacer.com", "https://crewspace.s3.ap-northeast-2.amazonaws.com/dummy/profile_suhan.png", "김수한", false);
             memberRepository.save(member11);
 
-            // 시연자
-            Member member12 = new Member("12", "one9872@naver.com", "https://crewspace.s3.ap-northeast-2.amazonaws.com/dummy/profile_jieun.png", "테스트 유저", false);
-            memberRepository.save(member12);
-
-            Member member13 = new Member("13", "aa9919@naver.com", "카카오 이미지-테스트 유저", "테스트 유저", false);
-            memberRepository.save(member13);
         }
 
         // 동아리 생성(13) - 동아리 배너, 이미지 삽입
